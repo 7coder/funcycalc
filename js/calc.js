557e1aa8e9e6cf145c8a63ba6066fcbb459d68ff
7coder <7code@mxt-c.com>
2016-11-02 19:40:04 +0400

    bugFix #1

--- js/calc.js
+++ js/calc.js
@@ -10,7 +10,8 @@ window.onload = function(){
 		percent = doc.querySelectorAll('.percent'),
 		showSign = doc.getElementById('calc_operation'),
 		eventItem = doc.getElementById('event_item'),
-		action = firstArgument = secondArgument = "",
+		onOff = doc.getElementById('check'),
+		action = firstArgument = secondArgument = calcValue.innerHTML = "",
 		resultOverall = 0;
 
 	//Function "Do another action after result"
@@ -31,216 +32,225 @@ window.onload = function(){
 		
 	};
 
-	// Initialization of number buttons
+	onOff.onchange = function (){
 
-	for (var i = 0; i < digits.length; i++) {	
+		if (onOff.checked){
 
-			digits[i].onclick = function(){
-
-				var digitsShow = this.innerHTML;
-
-				//Checking for available results
-				if(showSign.innerHTML === "="){
-					clearAll();
-				};
-
-				if (action === ""){
-	
-					firstArgument += digitsShow;
-
-					//Limit of numbers in Calculator Display for FirstArgument
-
-					if(firstArgument.length < 7){
-						calcValue.innerHTML = firstArgument; 	
-					} else{
-						calcValue.innerHTML = firstArgument.substring(0,7)
-					};
-
-				} else {
-					secondArgument += digitsShow;
-
-					//Limit of numbers in Calculator Display for SecondArgument
-
-					if(secondArgument.length < 7){
-						calcValue.innerHTML = secondArgument; 	
-					} else{
-						calcValue.innerHTML = secondArgument.substring(0,7)
-					};
-				};
-
-			};
-	};
+			calcValue.innerHTML = 0;
 			
-	// Initialization of actions buttons
-	for (var i = 0; i < actions.length; i++) {	
+			// Initialization of number buttons
 
-			actions[i].onclick = function(){
+			for (var i = 0; i < digits.length; i++) {	
 
-				action = this.getAttribute('data-value');	
+					digits[i].onclick = function(){
 
-				if(resultOverall !== undefined && secondArgument !== ''){
-					resultToFirstArgument (resultOverall);
-				};
+						var digitsShow = this.innerHTML;
 
-				//Show Sign value in Div container, when U clicked on button
+						//Checking for available results
+						if(showSign.innerHTML === "="){
+							clearAll();
+						};
 
-				switch (action){
-					case "plus" : {
-						showSign.innerHTML = "+";
-						break;
-					}
-					case "minus" : {
-						showSign.innerHTML = "-";
-						break;
-					}
-					case "multiply" : {
-						showSign.innerHTML = "x";
-						break;
-					}
-					case "divide" : {
-						showSign.innerHTML = "&#247;";
-						break;
-					}
-					case "sqrt" : {
-						showSign.innerHTML = "&radic;";
-						break;
-					}
-					default: {
-						showSign.innerHTML = "";
-						break;
-					}
-				};
+						if (action === ""){
+			
+							firstArgument += digitsShow;
+
+							//Limit of numbers in Calculator Display for FirstArgument
+
+							if(firstArgument.length < 7){
+								calcValue.innerHTML = firstArgument; 	
+							} else{
+								calcValue.innerHTML = firstArgument.substring(0,7)
+							};
+
+						} else {
+							secondArgument += digitsShow;
+
+							//Limit of numbers in Calculator Display for SecondArgument
+
+							if(secondArgument.length < 7){
+								calcValue.innerHTML = secondArgument; 	
+							} else{
+								calcValue.innerHTML = secondArgument.substring(0,7)
+							};
+						};
+
+					};
 			};
-	};
+					
+			// Initialization of actions buttons
+			for (var i = 0; i < actions.length; i++) {	
 
-	// Initialization of button of dign value
-	for (var i = 0; i < sign.length; i++) {	
+					actions[i].onclick = function(){
 
-			sign[i].onclick = function(){
+						action = this.getAttribute('data-value');	
 
-				if (firstArgument !== "0" && secondArgument === ""){
-					firstArgument = firstArgument * (-1);
-					calcValue.innerHTML = firstArgument;
-				} else {
-					secondArgument = secondArgument * (-1);
-					calcValue.innerHTML = secondArgument;
-				}
-				
+						if(resultOverall !== undefined && secondArgument !== ''){
+							resultToFirstArgument (resultOverall);
+						};
+
+						//Show Sign value in Div container, when U clicked on button
+
+						switch (action){
+							case "plus" : {
+								showSign.innerHTML = "+";
+								break;
+							}
+							case "minus" : {
+								showSign.innerHTML = "-";
+								break;
+							}
+							case "multiply" : {
+								showSign.innerHTML = "x";
+								break;
+							}
+							case "divide" : {
+								showSign.innerHTML = "&#247;";
+								break;
+							}
+							case "sqrt" : {
+								showSign.innerHTML = "&radic;";
+								break;
+							}
+							default: {
+								showSign.innerHTML = "";
+								break;
+							}
+						};
+					};
 			};
-	};
 
-	//Initialization of percent button
-	for (var i = 0; i < percent.length; i++) {	
+			// Initialization of button of dign value
+			for (var i = 0; i < sign.length; i++) {	
 
-			percent[i].onclick = function(){
+					sign[i].onclick = function(){
 
-				percent = this.getAttribute('data-value');
-
-				if (firstArgument !== "0" && secondArgument !== "0"){
-					secondArgument = (firstArgument * secondArgument)/100;
-				};
-
-				if (percent[i]){
-					showSign.innerHTML = "&#37;";
-				};
-				
-			};
-	};
+						if (firstArgument !== "0" && secondArgument === ""){
+							firstArgument = firstArgument * (-1);
+							calcValue.innerHTML = firstArgument;
+						} else {
+							secondArgument = secondArgument * (-1);
+							calcValue.innerHTML = secondArgument;
+						}
 						
-
-	// Initialization of Math operations: +, /, -, *, sqrt
-	for (var i = 0; i < result.length; i++) {	
-
-			result[i].onclick = function(){
-
-				switch (action){
-					case "plus" : {
-						resultOverall = Number(firstArgument) + Number(secondArgument);
-						showSign.innerHTML = "=";
-						break;
-					}
-					case "minus" : {
-						resultOverall = Number(firstArgument) - Number(secondArgument); 
-						showSign.innerHTML = "=";
-						break;
-					}
-					case "multiply" : {
-						resultOverall = Number(firstArgument) * Number(secondArgument); 
-						showSign.innerHTML = "=";
-						break;
-					}
-					case "divide" : {
-						resultOverall = Number(firstArgument) / Number(secondArgument); 
-						showSign.innerHTML = "=";
-						break;
-					}
-					case "sqrt" : {
-						resultOverall = Math.sqrt(Number(firstArgument)); 
-						showSign.innerHTML = "=";
-						break;
-					}
-					default: {
-						resultOverall = 0;
-						showSign.innerHTML = '';
-						break;
-					}
-				};
-				
-				calcValue.innerHTML = (Math.round(resultOverall * 1000)/1000);
-
-				// BigChecker values. JS Souce.
-				
-				if(firstArgument.length > 7 && firstArgument.length <= 9){
-					
-					calcValue.className = 'from7to9';
-
-				} else if (firstArgument.length > 9 && firstArgument.length <= 13){
-					
-					calcValue.className = 'from9to13';
-
-				};
-
-
-				//Check the results
-
-				if (resultOverall === Infinity || resultOverall === -Infinity){
-					
-					// Add 'animate_top' css class for popup block
-
-					eventItem.classList.add('animate_top', 'divide_on_zero');
-					calcValue.innerHTML = '<div class=\"event_text\">Please push the \"C\" to stop<br/>  annoy the angry granny!</div>';
-					firstArgument = '';
-
-				} else if (resultOverall === 707){
-
-					// Add 'animate_right' css class for popup block
-
-					eventItem.classList.add('animate_right', 'lol');
-					calcValue.innerHTML = 'LOL!';
-					firstArgument = '';
-
-				} else if (calcValue.innerHTML.length > 13){
-
-					// Add 'animate_top' css class for popup block
-
-					eventItem.classList.add('animate_top', 'too_long');
-					calcValue.innerHTML = '<div class=\"event_text\">Push the \"C\" button<br/> to wink to the pretty girl!</div>';
-
-				};
-				
+					};
 			};
-	};
 
+			//Initialization of percent button
+			for (var i = 0; i < percent.length; i++) {	
 
-	// Initialization of C button. Reset all values.
+					percent[i].onclick = function(){
 
-	for (var i = 0; i < reset.length; i++) {	
+						percent = this.getAttribute('data-value');
 
-			reset[i].onclick = function(){
-				clearAll();
+						if (firstArgument !== "0" && secondArgument !== "0"){
+							secondArgument = (firstArgument * secondArgument)/100;
+						};
+
+						if (percent[i]){
+							showSign.innerHTML = "&#37;";
+						};
+						
+					};
 			};
+								
+			// Initialization of Math operations: +, /, -, *, sqrt
+			for (var i = 0; i < result.length; i++) {	
+
+					result[i].onclick = function(){
+
+						switch (action){
+							case "plus" : {
+								resultOverall = Number(firstArgument) + Number(secondArgument);
+								showSign.innerHTML = "=";
+								break;
+							}
+							case "minus" : {
+								resultOverall = Number(firstArgument) - Number(secondArgument); 
+								showSign.innerHTML = "=";
+								break;
+							}
+							case "multiply" : {
+								resultOverall = Number(firstArgument) * Number(secondArgument); 
+								showSign.innerHTML = "=";
+								break;
+							}
+							case "divide" : {
+								resultOverall = Number(firstArgument) / Number(secondArgument); 
+								showSign.innerHTML = "=";
+								break;
+							}
+							case "sqrt" : {
+								resultOverall = Math.sqrt(Number(firstArgument)); 
+								showSign.innerHTML = "=";
+								break;
+							}
+							default: {
+								resultOverall = 0;
+								showSign.innerHTML = '';
+								break;
+							}
+						};
+						
+						calcValue.innerHTML = (Math.round(resultOverall * 1000)/1000);
+
+						// BigChecker values. JS Souce.
+						
+						if(calcValue.innerHTML.length > 7 && calcValue.innerHTML.length <= 10){
+							
+							calcValue.className = 'from7to10';
+
+						} else if (calcValue.innerHTML.length > 10 && calcValue.innerHTML.length <= 13){
+							
+							calcValue.className = 'from10to13';
+
+						};
+
+
+						//Check the results
+
+						if (resultOverall === Infinity || resultOverall === -Infinity){
+							
+							// Add 'animate_top' css class for popup block
+
+							eventItem.classList.add('animate_top', 'divide_on_zero');
+							calcValue.innerHTML = '<div class=\"event_text\">Please push the \"C\" to stop<br/>  annoy the angry granny!</div>';
+							firstArgument = '';
+
+						} else if (resultOverall === 707){
+
+							// Add 'animate_right' css class for popup block
+
+							eventItem.classList.add('animate_right', 'lol');
+							calcValue.innerHTML = 'LOL!';
+							firstArgument = '';
+
+						} else if (calcValue.innerHTML.length > 13){
+
+							// Add 'animate_top' css class for popup block
+
+							eventItem.classList.add('animate_top', 'too_long');
+							calcValue.innerHTML = '<div class=\"event_text\">Push the \"C\" button<br/> to wink to the pretty girl!</div>';
+
+						};
+						
+					};
+			};
+
+			// Initialization of C button. Reset all values.
+
+			for (var i = 0; i < reset.length; i++) {	
+
+					reset[i].onclick = function(){
+						clearAll();
+					};
+			};
+
+		} else {
+			calcValue.innerHTML = '';
+		};
+
 	};
-				
 
 };
 
