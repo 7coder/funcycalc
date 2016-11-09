aec8135f4169afea0d3ee1b0629a251d6baf693c
7coder <7code@mxt-c.com>
2016-11-09 14:55:17 +0400

    Update notecalc functions

--- js/note.js
+++ js/note.js
@@ -3,56 +3,80 @@ $(function () {
 	//Cache DOM
 	var $fCalcValue = $('#calc_value'),
 		$noteContainer = $('#note_container'),
-		$note_overall = $('#note_overall')
-		$overall = 0;
+		$note_overall = $('#note_overall'),
+		noteItemList = {},
+		overall = counterId = 0;
 
+
+	//Item line Mustach.js template
 	var noteLineTemplate = '' +
-	'<div class="note_item_wrap clearfix">' + 
+	'<div data-id="{{id}}" class="note_item_wrap clearfix">' + 
 	'<div class="note_item_value">{{value}}</div>' +
 	'<div class="note_item_decs">{{desc}}</div>' +
 	'<button class="remove">X</button></div>';
 
-	function addEntry(noteItem){
-		$noteContainer.append(Mustache.render(noteLineTemplate, noteItem));
+	// Initialization of Mustach.js
+	function addEntry(id, noteItem){
+
+		//Add ID to Template Mustach.js
+		var objectAssign = Object.assign({id: id}, noteItem);
+		$noteContainer.append(Mustache.render(noteLineTemplate, objectAssign));
 	};
 
+
+	//New Object NoteItem
+	var createNoteItem = function(value, desc){
+		return {
+				value: value,
+				desc: desc,
+				};
+	};
+
+	// Add lines to array
+	function addNoteItemToList(id, noteItem){
+		noteItemList[id] = noteItem;
+	};
+
+	// Initialization of Send Value button
 	$('#button_send').on('click', function(){
+		var noteItem, 
+			newId;
 
-		var noteItem = {
-			id: 0,
-			value: $fCalcValue.html(),
-			desc: 'Description',
-			},
-			$noteItemElement = $('.note_item_wrap');
-
-		if ($noteItemElement.length >= 10){
-
+		// The limit on the number of lines
+		if ($('.note_item_wrap').length >= 10){
 			return;
-
 		} else{
-			addEntry(noteItem);
-
+			noteItem = createNoteItem($fCalcValue.html(), 'Description');
+			newId = counterId++;
+		
+			addNoteItemToList(newId, noteItem);
+			addEntry(newId, noteItem);
 		};
+		
+		overall += Number(noteItem.value);
 
-		$overall += Number(noteItem.value);
+		$note_overall.html(overall);
+	});
 
-		$note_overall.html($overall);
-
-		$('.note_item_wrap').on('click', '.remove', function() {
+	// Initialization of remove button
+		$noteContainer.on('click', '.note_item_wrap .remove', function() {
 			
-			var $itemWrap = $(this).closest('div');
-			var $overallTotal = 0;
+			var $self = $(this),
+				$itemWrap = $self.closest('.note_item_wrap'),
+				wrapperId = $itemWrap.data('id'),
+				wrapperValue = noteItemList[wrapperId]['value'];
+				
 			
-			$itemWrap.fadeOut(200, function() {
-				$(this).remove();	
+			$itemWrap.fadeOut(100, function() {
+				$itemWrap.remove();	
+				delete noteItemList[wrapperId];
+				console.log(noteItemList);
 			}); 
-			
-			$overallTotal = $overall - Number(noteItem.value);	
 
-			$note_overall.html($overallTotal);
-		});
+			overall -= Number(wrapperValue);
+
+			$note_overall.html(overall);
 
 	});
 
-	
 });
