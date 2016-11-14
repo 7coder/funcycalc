3a2e8f8aaea1c547486a583769242dadef6167a2
7coder <7code@mxt-c.com>
2016-11-14 16:56:22 +0400

    Add line edit functions, add new line, and fix some bugs

--- js/note.js
+++ js/note.js
@@ -4,28 +4,10 @@ $(function () {
 	var $fCalcValue = $('#calc_value'),
 		$noteContainer = $('#note_container'),
 		$noteOverall = $('#note_overall'),
+		noteLineTemplate = $('#note_line_template').html(),
 		noteItemList = {},
 		overall = counterId = 0;
 		
-
-
-	//Item line Mustach.js template
-	var noteLineTemplate = '' +
-	'<div data-id="{{id}}" class="note_item_wrap add_new clearfix">' + 
-		'<div class="note_item_value">{{value}}</div>' +
-		'<div class="note_item_decs">' + 
-			'<div class="desc_text">{{desc}}</div>' + 
-			'<div class="desc_text_edit"><input type="text" placeholder="Description" class="desc_input" value=""></div>' + 
-		'</div>' +
-		'<div class="note_controls clearfix">' + 
-			'<div class="save_cancel">' + 
-				'<button class="save">Save</button>' + 
-				'<button class="cancel">Cancel</button>' + 
-			'</div>' + 
-			'<button class="edit">Edit</button>' + 
-			'<button class="remove">Delete</button>' +
-		'</div>' + 
-	'</div>';
 
 	// Initialization of Mustach.js
 	function addEntry(id, noteItem){
@@ -44,17 +26,31 @@ $(function () {
 				};
 	};
 
+
 	// Add lines to array
 	function addNoteItemToList(id, noteItem){
 		noteItemList[id] = noteItem;
 	};
 
+
 	//The number of free entries
 	function showFreeEntries(){
 		var entriesNumbers = Number($('#note_have_entries').html());
+		
 		entriesNumbers = 10 - $('.note_item_wrap').length;
 		$('#note_have_entries').html(entriesNumbers);
 	};
+
+
+	//Limit characters in input
+	function doLimitInput(field, maxChar){
+		var inputClass= $(field),
+			inputVal = inputClass.val(),
+			symbols = parseInt(maxChar - inputVal.length);
+			
+			$('.limit_characters').html(symbols);
+	};
+
 
 	// Initialization of Send Value button
 	$('#button_send').on('click', function(){
@@ -67,16 +63,27 @@ $(function () {
 		} else{
 			noteItem = createNoteItem($fCalcValue.html(), 'Description');
 			newId = counterId++;
+
+			if($fCalcValue.html() === ''){
+				noteItem['value'] = 0;
+			}
 		
 			addNoteItemToList(newId, noteItem);
 			addEntry(newId, noteItem);
 		};
-		
+
 		showFreeEntries();		
 
 		overall += Number(noteItem.value);
 
 		$noteOverall.html(overall);
+
+
+		//Limit input
+		$('.desc_input').on('keyup', function(){
+			doLimitInput(this, 50);
+		});
+
 	});
 
 	// Initialization of remove button
@@ -90,9 +97,8 @@ $(function () {
 		$itemWrap.fadeOut(100, function() {
 			$itemWrap.remove();	
 			delete noteItemList[wrapperId];
+			showFreeEntries();
 		}); 
-		
-		showFreeEntries();
 
 		overall -= Number(wrapperValue);
 
@@ -100,33 +106,42 @@ $(function () {
 
 	});
 
+	
+
 	// Initialization of save button
 	$noteContainer.on('click', '.note_item_wrap .save', function() {
 		
-		var descInputValue = $(this).parent().parent().parent().find('.desc_input').val();
+		var $itemWrap = $(this).closest('.note_item_wrap'),
+			descInputValue = $itemWrap.find('.desc_input').val(),
+			wrapperId = $itemWrap.data('id');
 
-		if (descInputValue === ''){
-			$(this).parent().parent().parent().find('.desc_text').html('...');
-		} else {
-			$(this).parent().parent().parent().find('.desc_text').html(descInputValue);
-		};
+			if (descInputValue === ''){
+				noteItemList[wrapperId]['desc'] = '...';
+				$itemWrap.find('.desc_text').html('...');
 
-		$('.note_item_wrap').removeClass('add_new').removeClass('edit_item');
+			} else {
+				noteItemList[wrapperId]['desc'] =  descInputValue;
+				$itemWrap.find('.desc_text').html(descInputValue);
+			};
+
+		$itemWrap.removeClass('add_new').removeClass('edit_item');
 
 	});
 
 	// Initialization of edit button
-	$noteContainer.on('click', '.note_item_wrap .edit', function() {
+	$noteContainer.on('click', '.note_item_wrap .edit_desc', function() {
 
-		$('.note_item_wrap').addClass('edit_item');
+
+		$(this).closest('.note_item_wrap').addClass('edit_item');
 	
 	});
 
 	// Initialization of cancel button
 	$noteContainer.on('click', '.note_item_wrap .cancel', function() {
 
-		$('.note_item_wrap').removeClass('edit_item');
+		$(this).closest('.note_item_wrap').removeClass('edit_item').removeClass('add_new');
 	
 	});
+
 
 });
